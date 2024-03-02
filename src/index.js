502fa7e3495ba4852c4eabcd8a5bc8a8533b729b
Ahmad Masud <ahmadmasud25@hotmail.com>
2024-03-01 19:59:14 -0800

    added basename to Browser Router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter basename="/TempTalk">
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
